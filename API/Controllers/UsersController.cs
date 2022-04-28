bfb791b18b81d5d1da21a411c1c11734cc5c9b75
Matthew Priebe <priebemb@gmail.com>
2022-04-28 16:57:32 -0500

    First Commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+        //Returns list of all users in the database asynchronously.
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            //await keyword needed for async requests
+            return await _context.Users.ToListAsync();
+        }
+        
+        //Should use asynchronous code whenever possible for scalability reasons.
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
