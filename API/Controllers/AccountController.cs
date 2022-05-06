652b25fce802bc86cefe86cb8e141d60bfbf206e
Matthew Priebe <priebemb@gmail.com>
2022-05-06 14:38:37 -0500

    Added Login DTo and modified Account Controller.

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -35,7 +35,24 @@ public class AccountController : BaseApiController
 
         return user;
     }
-    
+
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> Login(LoginDTO loginDto)
+    {
+        var user = await _context.Users.SingleOrDefaultAsync(user => user.UserName == loginDto.UserName);
+
+        if (user == null) return Unauthorized("Invalid Username");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+        }
+        return user;
+    }
+
     private async Task<bool> UserExists(string username)
     {
         return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
